62b14eb619e28dd3d54f071f50d11cc01e70436e
christianonoh <onohchibyk@gmail.com>
2023-05-24 00:20:17 +0100

    Add functionality to SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BsSearch } from 'react-icons/bs';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { selectCity } from '../redux/weather/weatherSlice';
 
-const SearchBar = () => (
-  <div className="search">
-    <input type="text" id="search" placeholder="Enter city name..." />
-    <button type="button">
-      <BsSearch />
-    </button>
-  </div>
-);
+const SearchBar = () => {
+  const [searchValue, setSearchValue] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleChange = (event) => setSearchValue(event.target.value);
+  const handleSubmit = () => {
+    console.log(searchValue);
+    dispatch(selectCity(searchValue));
+    navigate(`/details/${searchValue}`);
+  };
+  return (
+    <div className="search">
+      <input type="text" id="search" value={searchValue} placeholder="Enter city name..." onChange={handleChange} />
+      <button type="button" onClick={handleSubmit}>
+        <BsSearch />
+      </button>
+    </div>
+  );
+};
 
 export default SearchBar;
